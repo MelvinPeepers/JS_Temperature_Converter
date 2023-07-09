b896fb78bc8631f69479194a86855615c0d5b190
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2023-07-09 09:30:35 -0400

    working on JS

--- js.js
+++ js.js
@@ -1,45 +1,31 @@
 //need to:
 //get value from input, perform formulas, display new result
-const celciusInput = document.querySelector('.celsius>input')
+const celsiusInput = document.querySelector('.celsius>input')
 const fahrenheitInput = document.querySelector('.fahrenheit>input')
 const kelvinInput = document.querySelector('.kelvin>input')
-//IF F IS INPUT
-//
-//F to K
-// (f - 32) * 5/9 + 273.15
+
+// Convert Fahrenheit to Celsius and Kelvin
 function convertFahrenheit() {
-  kelvinInput.value = (parseFloat(fahrenheitInput.value) - 32) * 5/9 + 273.15;
-  celciusInput.value = (parseFloat(fahrenheitInput.value) -32) * 5/9
+  kelvinInput.value = parseFloat(fahrenheitInput.value) - 32 * 5/9 + 273.15;
+  celsiusInput.value = parseFloat(fahrenheitInput.value) -32 * 5/9;
 }
 fahrenheitInput.addEventListener('input', convertFahrenheit)
-//
-//F to C
-// (f - 32) * 5/9
+
+// Convert Celsius to Fahrenheit and Kelvin
 function convertCelsius() {
-    fahrenheitInput.value = (parseFloat (celsiusInput.value) - 32 * 5/9);
-    kelvinInput.value = (parseFloat (celsiusInput.value) - 273.15);
+    fahrenheitInput.value = parseFloat (celsiusInput.value) * 9/5 + 32;
+    kelvinInput.value = parseFloat (celsiusInput.value) + 273.15;
 }
 celsiusInput.addEventListener('input', convertCelsius)
-//IF C IS INPUT
-//
-//C to K
-// c + 273.15
-//
-//C to F
-// (c + 32) * 1.8
+
+// Convert Kelvin to Fahrenheit and Celsius
+function convertKelvin() {
+  fahrenheitInput.value = parseFloat (kelvinInput.value) - 273.15 * 1.8 + 32;
+  celsiusInput.value = parseFloat(kelvinInput.value) - 273.15;
+}
+kelvinInput.addEventListener('input', convertKelvin)
 
 
-//IF K IS INPUT
-//
-//K to F
-// (k + 32) / 5.9
-//
-//K to C
-// k - 273.15
-
-
-// 23hajkk - 23
-// ahghk23ahhkd 23
 
 
 
