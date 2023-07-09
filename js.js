e6e37a59df951e21cb2ae73ae6548487abc6dd03
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2023-07-09 10:38:24 -0400

    some styling some JS

--- js.js
+++ js.js
@@ -4,23 +4,29 @@ const celsiusInput = document.querySelector('.celsius>input')
 const fahrenheitInput = document.querySelector('.fahrenheit>input')
 const kelvinInput = document.querySelector('.kelvin>input')
 
-// Convert Fahrenheit to Celsius and Kelvin
-function convertFahrenheit() {
-  kelvinInput.value = parseFloat(fahrenheitInput.value) - 32 * 5/9 + 273.15;
-  celsiusInput.value = parseFloat(fahrenheitInput.value) -32 * 5/9;
-}
-fahrenheitInput.addEventListener('input', convertFahrenheit)
+
 
 // Convert Celsius to Fahrenheit and Kelvin
 function convertCelsius() {
-    fahrenheitInput.value = parseFloat (celsiusInput.value) * 9/5 + 32;
-    kelvinInput.value = parseFloat (celsiusInput.value) + 273.15;
+  fahrenheitInput.value = (parseFloat (celsiusInput.value) * 9/5) + 32;
+  kelvinInput.value = parseFloat (celsiusInput.value) + 273.15;
 }
 celsiusInput.addEventListener('input', convertCelsius)
 
+
+
+// Convert Fahrenheit to Celsius and Kelvin
+function convertFahrenheit() {
+  kelvinInput.value = (parseFloat(fahrenheitInput.value) - 32) * 5/9 + 273.15;
+  celsiusInput.value = (parseFloat(fahrenheitInput.value) -32) * 5/9;
+}
+fahrenheitInput.addEventListener('input', convertFahrenheit)
+
+
+
 // Convert Kelvin to Fahrenheit and Celsius
 function convertKelvin() {
-  fahrenheitInput.value = parseFloat (kelvinInput.value) - 273.15 * 1.8 + 32;
+  fahrenheitInput.value = (parseFloat (kelvinInput.value) - 273.15) * 9/5 + 32;
   celsiusInput.value = parseFloat(kelvinInput.value) - 273.15;
 }
 kelvinInput.addEventListener('input', convertKelvin)
